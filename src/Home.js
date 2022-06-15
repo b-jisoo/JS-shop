8df536ffc052c8f070fdd2b24fc8d09d20db9f0d
B-jisoo <mrk334kr@naver.com>
2022-06-15 09:12:18 +0900

    home-priduct
    home css작성

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import HomeProduct from "./component/Home-product";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className="home">
-      <div>
+    <div className={styles.home}>
+      <div className={styles.home_productList}>
         <HomeProduct />
+        <HomeProduct />
+        <HomeProduct />
+        <HomeProduct />
+        <HomeProduct />
+        <HomeProduct />
+        <HomeProduct />
+        <HomeProduct />
+      </div>
+      <div className={styles.homeFooter}>
+        {/* TODO 여기는 페이지 표시하는 공간 */}
       </div>
     </div>
   );
